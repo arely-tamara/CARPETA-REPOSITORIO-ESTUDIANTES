d3b11fe284b76e11afe08afc11b13b4e96c56538
camilasepulvedab <camila.sepulveda5@mail.udp.cl>
2026-03-27 10:18:00 -0300

    Create P5.js

---
+++ 30-camila-sepulveda/P5.js
@@ -0,0 +1,22 @@
+# P5.js
+
+Algoritmo: secuencia de instrucciones, paso a paso, logicas, finitas, definidas,ordenadas y precisión.
+
+## estructura
+
+siempre debe haber un imput: información o ingredientes o urencilios para hacer un prudcto.
+proceso: lista de instrucciones para desdarrolar el proyecto, conjunto con los ingredientes. 
+y output:
+
+## diagramas de fkujo: 
+representación gráfica de un algoritmo o de los pasos de un proceso. herramienta de planificación para visualizar la logica de un programa antes de escribir una solo linea de codigo,. se usan componentes Estandar.
+ej: la lampara no funciona, esta enchufada?, esta quemada la ampolleta?; comprar una lampara. 
+
+
+## lenguaje de programación
+
+existen entre 700 y 900 lenguajes de programación.
+los mas usados son Python, java, c#, javascript,typescript,PHP,ruby,C,rust, entre otros. 
+
+## P5.js
+usa libreria de Javascript, pero mas simplificada, para poder manejarla de una manera mas sencilla.
